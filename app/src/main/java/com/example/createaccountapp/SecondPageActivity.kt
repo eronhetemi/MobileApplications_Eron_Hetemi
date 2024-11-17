21bb5ab686be96a3e9113802e88893d7f7369a6b
eronhetemi <ehetemi23@gmail.com>
2024-11-17 19:36:53 +0100

    Assigment 3 Commit

--- app/src/main/java/com/example/createaccountapp/SecondPageActivity.kt
+++ app/src/main/java/com/example/createaccountapp/SecondPageActivity.kt
@@ -1,11 +1,24 @@
 package com.example.createaccountapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.createaccountapp.databinding.ActivitySecondPage2Binding
 
 class SecondPageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondPage2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second_page2)
+        binding = ActivitySecondPage2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Click listener for "Log In"
+        binding.alreadyMemberTextView.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
